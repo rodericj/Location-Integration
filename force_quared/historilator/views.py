5442369116ca62e36166c8b873b0ae07b670c57f
Roderic Campbell <roderic@gmail.com>
2009-08-19 00:21:42 -0500

    checking up

--- force_quared/historilator/views.py
+++ force_quared/historilator/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
+import random
+import urllib
+import time
 import logging
 
 # Create your views here.
@@ -14,6 +17,21 @@ def start(request):
 	ret = {}
 	ret['username'] = request.user.username
 	return render_to_response('profile.html', ret)
+
+def authorizeSource(request):
+
+	#username, password, type
+	#foursquare auth info
+	oauth_consumer_key = '0bf482ea6ad98dc8fa2e668575c0eaba04a5ce475'
+	oauth_token = 'ff6729db73f8668c32e1419909e64c25'
+
+	data = {}
+	data['oauth_token'] = oauth_token
+	data['oauth_consumer_key'] = oauth_consumer_key
+	foursquare = 'http://api.playfoursquare.com/v1/authexchange'
+	return HttpResponsePermanentRedirect(foursquare)
+	
+	
 
 def weblogin(request):
 	emailAddress = request.POST['emailaddress']
@@ -67,10 +85,26 @@ def register(request):
 	
 	return HttpResponseRedirect(dest+'?ret='+ret['registerret'])
 
+def authorize(request):
+	None
+
 def addServices(request):
 	
 	ret = {}
-	ret['username'] = request.user.username
+	username = request.user.username
+	nonce = int(random.random()*100000000)
+	timestamp = int(time.time())
+	key = '52e6db5e1d8bd8c481e8e1e3f798652004a7fbfc8'
+	secret = '57c1aac04b076d8743664e2d935da13b'
+
+	ret['username'] = username
+	ret['nonce'] = nonce
+	ret['timestamp'] = timestamp
+	ret['key'] = key
+	ret['secret'] = secret
+	link = "http://api.playfoursquare.com/oauth/request_token?oauth_consumer_key="+key+"&oauth_signature="+secret+"&oauth_nonce="+str(nonce)+"&oauth_timestamp="+str(timestamp)+"&oauth_version=1.0"
+
+	ret['link'] = link
 	return render_to_response('login.html', ret)
 
 def verifyRegisterParams(emailAddress, username, password, confirmpassword):
